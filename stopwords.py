748dfd1d20be902079a0fe1cdb5c91c089c925c8
Nstats <17801238715@163.com>
2018-12-17 18:55:48 +0800

    stopwords stats

--- stopwords.py
+++ stopwords.py
@@ -6,10 +6,10 @@ train_dir = './data/train.csv'
 
 df = pd.DataFrame(pd.read_csv(train_dir, engine='python'))
 
-question_text = [word_tokenize(line) for line in df['question_text'].values]
+question_text = [word_tokenize(line) for line in df['question_text'].fillna('').values]
 target = df['target']
 
-#example = {'word': {'total': 100, 'insincere': 2, 'ratio': 0.02}}
+# example = {'word': {'total': 100, 'insincere': 2, 'ratio': 0.02}}
 stoplist = stopwords.words('english')
 
 word_dict = {}
@@ -25,7 +25,7 @@ for stopword in stoplist:
 
 for word in word_dict:
     if word_dict[word]['insincere'] == 0:
-        word_dict[word]['ratio'] = 1000
+        word_dict[word]['ratio'] = 1e10
     else:
         word_dict[word]['ratio'] = (word_dict[word]['total'] - word_dict[word]['insincere'])/(word_dict[word]['insincere'])
 
@@ -33,6 +33,6 @@ sorted_word_dict = sorted(word_dict.items(), key=lambda item: item[1]['ratio'], 
 sorted_word_dict2 = sorted(word_dict.items(), key=lambda item: item[1]['insincere'], reverse=True)
 sorted_word_dict3 = sorted(word_dict.items(), key=lambda item: item[1]['total'], reverse=True)
 
-with open('./data/stopwords_stats.txt', 'w') as f:
-    for ele in sorted_word_dict2:
+with open('./data/word_stats/stopwords_stats.txt', 'w') as f:
+    for ele in sorted_word_dict:
         f.write(str(ele)+'\n')
